a7e77b366fc5b6f841ea6632dd62152bb3424812
Alphexus <52839154+Alphexus@users.noreply.github.com>
2020-09-08 17:13:29 -0400

    Create compression.py

---
+++ compression.py
@@ -0,0 +1,55 @@
+from symbols import collection
+
+
+def getUniqueSymbol(blacklist):
+	for symbol in collection.split():
+		if not symbol in blacklist:
+			return symbol
+
+	return None
+
+def findRepeated(string):
+  output = ""
+  for i in range(1, len(string)-1):
+    substr = string[:i]
+    if string.find(substr, i+1) != -1:
+      output = substr
+    else:
+      break
+
+  return output
+
+def getAllRepeateds(text):
+  output = []
+  while True:
+    repeated = findRepeated(text)
+    if repeated and len(repeated) > 1:
+      output.append(repeated)
+      text = text.replace(repeated, "")
+    elif len(text) > 1:
+      text = text[1:]
+    else:
+      break
+      
+  return output
+
+
+def compress(str):
+  compressionKey = {}
+  blacklist = []
+  duplicates = getAllRepeateds(str)
+  for dup in duplicates:
+	  newSymbol = getUniqueSymbol(blacklist)
+	  if newSymbol != None:
+		  compressionKey[newSymbol] = dup
+		  blacklist.append(newSymbol)
+		  str = str.replace(dup, newSymbol)
+  
+  return str, compressionKey
+
+   
+def decompress(str, key):
+  for s in key:
+    str=str.replace(s, key[s])
+	
+  return str
